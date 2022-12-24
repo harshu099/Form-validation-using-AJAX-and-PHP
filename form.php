fcb59e6fcba36583f1f1b88c477a3a521e737773
Kumar Harsh <harshkumar12799@gmail.com>
2022-12-24 19:22:14 +0530

    first implementation

---
+++ form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form action="">
+        <input type="hidden" name="">
+        <div>
+            <label for="">Name :</label>
+            <input type="text" id="name">
+        </div>
+        <div>
+            <label for="">Email :</label>
+            <input type="email" id="email">
+        </div>
+        <div>
+            <label for="">Website :</label>
+            <input type="text" id="website">
+        </div>
+        <div>
+            <label for="">Comment :</label>
+            <textarea name="" id="comment" cols="30" rows="10"></textarea>
+        </div>
+        <div>
+            <label for="">Gender :</label>
+            <input type="radio" name="gender" id="">Male
+            <input type="radio" name="gender" id="">Female
+        </div>
+
+    </form>
+</body>
+</html>
